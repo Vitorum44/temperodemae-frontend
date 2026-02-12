ec2ac67d09b58afd058b1af731d1eb6f139696e3
Vitorum44 <vit.emanueldev@gmail.com>
2026-02-12 04:21:32 -0300

    Update cliente.js

--- js/cliente.js
+++ js/cliente.js
@@ -585,13 +585,6 @@ function renderItems() {
   window.scrollTo(0, originalScroll); // 🔥 evita “pulo” de tela
 }
 
-const existingSections = document.querySelectorAll('.category-section');
-if (existingSections.length > 0) {
-  return;
-}
-
-
-
 function setupScrollSpy() {
   const sections = document.querySelectorAll('.category-section'); const navChips = document.querySelectorAll('#category-chips .chip');
   const observer = new IntersectionObserver((entries) => {
