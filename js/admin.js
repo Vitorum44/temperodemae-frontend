5dd008cb66ae9df39889544d499b5a8efe63d036
Vitorum44 <vit.emanueldev@gmail.com>
2026-04-25 03:25:25 -0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -306,7 +306,16 @@ Obrigado pela preferência!`;
 function renderOrders(orders) {
     ordersList.innerHTML = '';
     if (orders.length === 0) {
-        ordersList.innerHTML = '<div style="text-align:center; padding:40px; color:#9ca3af;">Nenhum pedido hoje...</div>';
+        ordersList.innerHTML = `
+          <div class="loader-wrapper">
+            <div class="loader">
+              <div class="bar1"></div><div class="bar2"></div><div class="bar3"></div>
+              <div class="bar4"></div><div class="bar5"></div><div class="bar6"></div>
+              <div class="bar7"></div><div class="bar8"></div><div class="bar9"></div>
+              <div class="bar10"></div><div class="bar11"></div><div class="bar12"></div>
+            </div>
+            <span>Nenhum pedido hoje...</span>
+          </div>`;
         return;
     }
 
