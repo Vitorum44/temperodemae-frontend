02c507f6ebd34632cd7796bd3cce0e60b5b6ad36
Vitorum44 <vit.emanueldev@gmail.com>
2026-04-25 03:24:51 -0300

    Update estoque.js

--- js/estoque.js
+++ js/estoque.js
@@ -663,7 +663,16 @@ async function carregarAcompanhamentos(produtoId){
   const box = document.getElementById("acomp-groups")
   if(!box) return
 
-  box.innerHTML = "Carregando..."
+  box.innerHTML = `
+    <div class="loader-wrapper">
+      <div class="loader">
+        <div class="bar1"></div><div class="bar2"></div><div class="bar3"></div>
+        <div class="bar4"></div><div class="bar5"></div><div class="bar6"></div>
+        <div class="bar7"></div><div class="bar8"></div><div class="bar9"></div>
+        <div class="bar10"></div><div class="bar11"></div><div class="bar12"></div>
+      </div>
+      <span>Carregando...</span>
+    </div>`
 
   try{
 
